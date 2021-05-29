cbc1295afbb8fed5faae5a8e7cef62f10e4c40fd
Yashira Perera <krishmal.yash@gmail.com>
2021-05-29 11:48:29 +0530

    Adding strapi backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '82e665529a052d34269f51ee090e9154'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '567bfd99-db0e-46ab-9ea7-03f119eb4e48'
+};
